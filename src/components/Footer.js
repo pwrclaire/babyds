39b997a2b7e67173fd61b8df592090f495f37ca1
Claire Peng <pwrclaire@gmail.com>
2020-11-20 12:58:11 -0800

    under construction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,3 @@
-import { Link } from 'gatsby'
 import React from 'react'
 
 const Footer = props => (
@@ -6,57 +5,36 @@ const Footer = props => (
     <section>
       <h2>Hours of Operation</h2>
       <ul className="info">
-        <li>Monday: 10-10</li>
-        <li>Tuesday: 10-10</li>
-        <li>Wednesday: 10-10</li>
-        <li>Thursday: 10-10</li>
-        <li>Friday: 10-10</li>
-        <li>Saturday: 10-10</li>
+        <li>Monday: 7-6:30</li>
+        <li>Tuesday: 7-6:30</li>
+        <li>Wednesday: 7-6:30</li>
+        <li>Thursday: 7-6:30</li>
+        <li>Friday: 7-6:30</li>
+        <li>Saturday: 9-5</li>
+        <li>Sunday: 9-5</li>
       </ul>
     </section>
     <section>
       <h2>Information</h2>
       <dl className="alt">
         <dt>Address</dt>
-        <dd>#11-29 W. Broadway &bull; Vancouver &bull; 1P1</dd>
+        <dd>#11-29 W. Broadway &bull; Vancouver &bull; V5Y 1P1</dd>
         <dt>Phone</dt>
-        <dd>(778) 000-0000</dd>
-        <dt>Email</dt>
-        <dd>
-          <a href="https://codebushi.com">information@untitled.tld</a>
-        </dd>
+        <dd>604-930-3423</dd>
       </dl>
       <ul className="icons">
         <li>
           <a
-            href="https://twitter.com/huntaroSan"
-            className="icon fa-twitter alt"
+            href="https://www.instagram.com/babydshomevancouver/"
+            className="icon fa-instagram alt"
           >
-            <span className="label">Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://codebushi.com" className="icon fa-facebook alt">
-            <span className="label">Facebook</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://codebushi.com" className="icon fa-instagram alt">
             <span className="label">Instagram</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/codebushi/gatsby-starter-stellar"
-            className="icon fa-github alt"
-          >
-            <span className="label">GitHub</span>
           </a>
         </li>
       </ul>
     </section>
     <p className="copyright">
-      &copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.
+      &copy; Baby D's 2020. Design: <a href="https://html5up.net">HTML5 UP</a>.
     </p>
   </footer>
 )
