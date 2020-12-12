99ce35028f2aa408eb009dc6e2ffc11db4c040e6
Claire Peng <pwrclaire@gmail.com>
2020-12-11 18:14:50 -0800

    edit price table

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -16,19 +16,19 @@ const Pricing = () => {
             <td>1 time drop in $24 (5hrs)</td>
           </tr>
           <tr>
-            <td>5 times $165 ($33)</td>
+            <td>5 times $170 ($34)</td>
             <td>5 times $115 ($23)</td>
           </tr>
           <tr>
-            <td>10 times $310 ($31)</td>
-            <td>10 times $217.50 (21.75)</td>
+            <td>10 times $320 ($32.50)</td>
+            <td>10 times $215 (21.50)</td>
           </tr>
           <tr>
-            <td>20 times $570 ($28.50) </td>
-            <td>20 times $405.00 (20.25)</td>
+            <td>20 times $610 ($30.50) </td>
+            <td>20 times $410 (20.50)</td>
           </tr>
           <tr>
-            <td>Monthly pass $520</td>
+            <td>Monthly pass $540 ($27)</td>
             <td></td>
           </tr>
         </tbody>
