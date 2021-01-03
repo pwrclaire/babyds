a7dd8354afee7fd4df8901f909b07daa0ba3c039
Claire Peng <pwrclaire@gmail.com>
2021-01-02 22:28:15 -0800

    remove excess content

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const Index = () => {
       <Nav sticky={stickyNav} />
 
       <div id="main">
-        <section id="intro" className="main">
+        <section id="intro" className="main special">
           <div className="spotlight">
             <div className="content">
               <header className="major">
@@ -56,58 +56,28 @@ const Index = () => {
             <div className="content">
               <header className="major">
                 <h2>Day Care</h2>
-                <h3>How is the day being spent in Baby D’s Home?</h3>
               </header>
-
-              <h3>Morning</h3>
+              <h2>Daycare In take Process: </h2>
               <p>
-                When our furry friends arrive, we realize that they may have
-                been too excited to do all their “business”. Soooo…out we go for
-                our first walk of the day. If it’s wet (really, Vancouver!) paws
-                and bellies get toweled off. Once that’s done, the dogs see
-                which of their friends have gotten there before them. The
-                running, the chasing, the playing that ensues; and we try to
-                capture all this in photos and little videos that we share on
-                Instagram throughout the day. The first 2-3 hours at Baby D’s
-                home are busy with these activities, and of course your babies
-                are rewarded with little training treats for being so well
-                behaved.
+                All dogs must have their 3rd set of shots and be spayed or
+                neutered (by 7 months of age). Book your free orientation and 3
+                hour trial for your fur baby (Available on Mondays, Fridays and
+                Saturdays). This allows you to tour the facility and your pup to
+                play and explore.
               </p>
-              <h3>Afternoon</h3>
-              <p>
-                Then around noon, lunch is served (in private) for those of our
-                friends that brought a “lunch box”. Once their bellies are full,
-                they go for another walk and often opt for a nap once they get
-                back. All that playing can be exhausting, don’t you know? Also,
-                some pups come for a half day around this time, so there’s more
-                friends to greet and sniff.
-                <br />
-                The afternoon is often lower key than the morning, but that
-                doesn’t mean that it’s any less fun. More walks and more playing
-                continue until it’s time to get picked up, so be prepared for
-                your furry companion to be tuckered out when you get home.
-              </p>
+              <h3>
+                Regular Pricing for Daycare (valid for 6 months and returning
+                clients would enjoy the old pricing for 2020!!)
+              </h3>
+              <button onClick={() => handlePriceClick()}>
+                <a className="icon fa-expand">
+                  {' '}
+                  {showPrice ? 'Hide Pricing Table' : 'Show Pricing Table'}
+                </a>
+              </button>
+              {showPrice ? <Pricing /> : ''}
             </div>
           </div>
-          <h2>Daycare In take Process: </h2>
-          <p>
-            All dogs must have their 3rd set of shots and be spayed or neutered
-            (by 7 months of age). Book your free orientation and 3 hour trial
-            for your fur baby (Available on Mondays, Fridays and Saturdays).
-            This allows you to tour the facility and your pup to play and
-            explore.
-          </p>
-          <h3>
-            Regular Pricing for Daycare (valid for 6 months and returning
-            clients would enjoy the old pricing for 2020!!)
-          </h3>
-          <button onClick={() => handlePriceClick()}>
-            <a className="icon fa-expand">
-              {' '}
-              {showPrice ? 'Hide Pricing Table' : 'Show Pricing Table'}
-            </a>
-          </button>
-          {showPrice ? <Pricing /> : ''}
         </section>
 
         <section id="grooming" className="main">
