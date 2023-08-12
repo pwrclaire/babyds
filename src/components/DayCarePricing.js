b31fde29168567ddbddda83070367e710a713ee8
Claire Peng <pwrclaire@gmail.com>
2023-08-11 22:12:57 -0700

    Update pricing

--- src/components/DayCarePricing.js
+++ src/components/DayCarePricing.js
@@ -12,30 +12,26 @@ const DayCarePricing = () => {
         </thead>
         <tbody>
           <tr>
-            <td>1 time drop in $39 (with discretion)</td>
-            <td>1 time drop in $28 (5hrs)</td>
+            <td>1 Day Pass $45</td>
+            <td>1 Day Pass $32</td>
           </tr>
           <tr>
-            <td>5 times $185 ($37)</td>
-            <td>5 times $135 ($27)</td>
+            <td>5 Day Package $215 ($43)</td>
+            <td>5 Day Package $155 ($31)</td>
           </tr>
           <tr>
-            <td>10 times $360 ($36)</td>
-            <td>10 times $260 ($26)</td>
+            <td>10 Day Package $410 ($41)</td>
+            <td>10 Day Package $290 ($29)</td>
           </tr>
           <tr>
-            <td>15 times $510 ($34)</td>
-            <td>15 times $360 ($24)</td>
-          </tr>
-          <tr>
-            <td>20 times $660 ($33) </td>
-            <td>20 times $460 ($23)</td>
+            <td>20 Day Package $760 ($38) </td>
+            <td>20 Day Package $250 ($26)</td>
           </tr>
         </tbody>
       </table>
       <h4>20% off for second dog (from same household)</h4>
       <h4>
-        Hourly rate / full day extension: $13
+        Hourly rate / full day extension: $15
         <br />
         Late Pickup: $15 for first 20 minutes, and $20 every 15 mins after
       </h4>
